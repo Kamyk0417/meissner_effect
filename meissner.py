e00700eb8e5bdd76dbcf804960157f32ff9292e8
Kamyk0417 <104413024+Kamyk0417@users.noreply.github.com>
2025-02-02 15:56:13 +0100

    Update meissner.py

--- meissner.py
+++ meissner.py
@@ -13,22 +13,21 @@ class Superconductor:
         self.H_c2 = H_c2
         self.H_c1 = H_c1
     
-    #sprawdza czy punkt (x,y,z) znajduje się wewnątrz nadprzewodnika 
-    def inside(self, x, y, z):
-        return np.sqrt(x**2+y**2+z**2) < self.radius
+    #sprawdza czy punkt znajduje się wewnątrz nadprzewodnika 
+    def inside(self, r):
+        return r < self.radius
     
 class TypeISc(Superconductor):
-    def magn_field(self, X, Y, Z, H_0):
-        #składowe pola magnetycznego (zał. pole ma tylko składową z, mnożenie przez 0 służy tylko temu żeby na wykresie 
-        #linie pola na zewnątrz nie zasłaniały nadprzewodnika), podział przestrzeni na punkty 
-        H_x = np.zeros_like(X)
-        H_y = np.zeros_like(Y)
-        H_z = 0*H_0*np.ones_like(Z)
+    def magn_field(self, R, Theta, Phi, H_0):
+        #składowe pola magnetycznego, podział przestrzeni na punkty 
+        H_r = np.zeros_like(R)
+        H_theta = np.zeros_like(Theta)
+        H_phi = np.zeros_like(Phi)
 
         #każdy punkt ma przypisane True lub False zależnie czy jest wewnątrz czy nie
-        inside = self.inside(X,Y,Z)
+        inside = self.inside(R)
         #każdy punkt wewnątrz ma przypisaną odległość od środka
-        dist = np.sqrt(X[inside]**2+Y[inside]**2+Z[inside]**2)
+        dist = R[inside]
         
         #rozłożenie równania Londonów (laplasjan(H) = H/lambda^2) na dwa równania pierwszego rzędu
         def london_eq(H, z):
@@ -50,40 +49,44 @@ class TypeISc(Superconductor):
                 H_ins[i] = sol[-1, 0]
 
         if H_0 < self.H_c1:
-            #analityczne rozwiązanie równania laplasjan(H) = H/lambda^2 i rozłożenie wyniku na składowe 
-            #H_ins = H_0*np.exp(-(self.radius-dist)/self.lambdaL)
-            H_x[inside] = H_ins * (X[inside]/dist)
-            H_y[inside] = H_ins * (Y[inside]/dist)
-            H_z[inside] = H_ins * (Z[inside]/dist)
+            H_r[inside] = H_ins
 
-        return H_x, H_y, H_z
+        return H_r,H_theta,H_phi
 
 def plot_magn_field(sc, H_0):
     #podział przestrzeni na punkty, podział na "osie" funkcją meshgrid i wywołanie metody magn_field
-    x = np.linspace(-7,7,30)
-    y = np.linspace(-7,7,30)
-    z = np.linspace(-7,7,30)
+    rd = np.linspace(0, sc.radius+2, 30)
+    alfa = np.linspace(0,2*np.pi,30)
+    beta = np.linspace(0,np.pi,30)
 
-    x1,y1,z1 = np.meshgrid(x,y,z)
-    H_x,H_y,H_z = sc.magn_field(x1,y1,z1, H_0)
+    rd,alfa,beta = np.meshgrid(rd,alfa,beta)
+    H_r,H_theta,H_phi = sc.magn_field(rd,alfa,beta, H_0)
+
+    #przejście do współrzędnych kartezjańskich
+    X = rd * np.sin(alfa) * np.cos(beta)
+    Y = rd * np.sin(alfa) * np.sin(beta)
+    Z = rd * np.cos(alfa)
+
+    H_x = H_r * np.sin(alfa) * np.cos(beta)
+    H_y = H_r * np.sin(alfa) * np.sin(beta)
+    H_z = H_r * np.cos(alfa)
 
     #wykreślenie w 3d wektorów pola
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-    ax.quiver(x1,y1,z1, H_x,H_y,H_z, normalize=False, length=0.05, alpha=0.3)
+    ax.quiver(X,Y,Z, H_x,H_y,H_z, normalize=False, length=0.05, alpha=0.3)
     
-    #przejście do współrzędnych sferycznych, dodanie na wykresie sfery o promieniu r reprezentującej nadprzewodnik
-    r = 7 
+    #dodanie sfery reprezentującej nadprzewodnik
+    r = sc.radius 
     theta = np.linspace(0, 2 * np.pi, 100)
     phi = np.linspace(0, np.pi, 50)
 
-
     theta, phi = np.meshgrid(theta, phi)
-    x = r * np.sin(phi) * np.cos(theta)
-    y = r * np.sin(phi) * np.sin(theta)
-    z = r * np.cos(phi)
+    x_s = r * np.sin(phi) * np.cos(theta)
+    y_s = r * np.sin(phi) * np.sin(theta)
+    z_s = r * np.cos(phi)
 
-    ax.plot_surface(x, y, z, color='b', alpha=0.1, edgecolor='r')
+    ax.plot_surface(x_s, y_s, z_s, color='b', alpha=0.1, edgecolor='r')
 
     ax.set_xlim(-10,10)
     ax.set_ylim(-10,10)
@@ -101,4 +104,3 @@ sc1 = TypeISc(radius, lambdaL, H_c1)
 #zadanie pola początkowego i narysowanie ostatecznego wykresu
 H_0 = 50
 plot_magn_field(sc1, H_0)
-
